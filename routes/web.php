ae4d9f8285d7de097d6a0c3947475460b1baa6a6
LLL <1401582905@qq.com>
2020-05-06 08:25:00 +0000

    用户个人页面原型

--- routes/web.php
+++ routes/web.php
@@ -34,3 +34,4 @@ Route::get('email/verify/{id}/{hash}', 'Auth\VerificationController@verify')->na
 Route::post('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
